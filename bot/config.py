667c673b60639f4f8b7c8c31c952e98ba7a64f59
krushovice <kickstar69@yandex.ru>
2023-03-28 13:58:21 +1000

    fix

--- bot/config.py
+++
@@ -1,4 +0,0 @@
-API_TOKEN = '5689414905:AAGS72UQfDuf-B-pbabipB-g7CxLDDcjGM0'
-admin_id = '1130398207'
-PAYMENTS_TOKEN = '1744374395:TEST:8b2e6a66fd0a1f861604'
-item_url = 'https://www.pngall.com/wp-content/uploads/13/Pay-PNG-Pic.png'
